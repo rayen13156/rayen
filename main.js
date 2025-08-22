9a0f1e75e02cdfd0c0ab36eca9c7a39283c216dc
rayen13156 <mohamedrayenouerghui@gmail.com>
2025-08-22 05:01:52 +0100

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.product-card button').forEach(btn => {
-  btn.addEventListener('click', () => {
-    btn.textContent = 'Added!';
-    setTimeout(() => btn.textContent = 'Add to Cart', 1000);
-  });
-});
